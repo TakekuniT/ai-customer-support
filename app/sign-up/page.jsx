a5c11ac04db76fe974796f6c9d8e53d4b2f258b8
amissimer <alivia.missimer04@gmail.com>
2024-08-07 21:05:26 -0400

    auth components + pages

--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -1,19 +1,22 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/router";
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, 
+  useSendEmailVerification, } from 'react-firebase-hooks/auth';
 import { auth } from '@/firebase';
 
 export default function Page() {
 
   const router = useRouter(); 
-  const [signInUserWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
+  const [createUser] = useCreateUserWithEmailAndPassword(auth);
+  const [sendEmailVerification] = useSendEmailVerification(auth);
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   
   const onSubmit = async () => {
-    await signInUserWithEmailAndPassword(email, password);
+    await createUser(email, password);
+    await sendEmailVerification();
     router.push('/');
   }
 
